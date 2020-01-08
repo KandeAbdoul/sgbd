8e1efa2e4216e674344030d5c38700903eab87ed
cheikmbacke <serignecheikhmbackethiaw@gmail.com>
2020-01-08 22:01:49 +0000

    Suppression du fichier LDD

--- LDD.py
+++
@@ -1,9 +0,0 @@
-from DB import Database
-import json
-from DB import Database
-##Fonction qui cree le fichier base de doneees
-
-
-##Fonction qui ajoute une table a une base de donnee
-def create_table():
-    pass
